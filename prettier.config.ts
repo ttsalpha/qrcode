b8b9d71ba3440f098c08145b2baf2edd9e1d8fc9
Son Tran <ttsalpha@icloud.com>
2026-05-15 05:18:22 +0700

    chore: init project config

---
+++ prettier.config.ts
@@ -0,0 +1,12 @@
+import { type Config } from 'prettier';
+
+const config: Config = {
+  singleQuote: true,
+  semi: true,
+  tabWidth: 2,
+  printWidth: 80,
+  trailingComma: 'all',
+  bracketSameLine: false,
+};
+
+export default config;
---
+++ tsup.config.ts
@@ -0,0 +1,11 @@
+import { defineConfig } from 'tsup';
+
+export default defineConfig({
+  entry: ['src/index.ts'],
+  format: ['esm', 'cjs'],
+  dts: true,
+  external: ['react', 'react-dom'],
+  target: 'es2018',
+  clean: true,
+  sourcemap: true,
+});
